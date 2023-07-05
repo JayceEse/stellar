cf87eb23fe36a5d520a2b4204362bfe72d3d4447
Jayce Ese <jayce.ese@gmail.com>
2023-07-05 13:16:14 +1000

    feat: Make navbar responsive

--- src/Pages/index.tsx
+++
@@ -1,9 +0,0 @@
-// Pages to be exported to App.tsx
-export { default as Home } from "./Home/Home";
-export { default as Plan } from "./Plan/Plan";
-export { default as Discover } from "./Discover/Discover";
-export { default as Check } from "./Check/Check";
-export { default as NoPage } from "./nopage/noPage";
-
-// Kitchen sink to export to app.tsx
-export { default as Kitchen } from "./KitchenSink/KitchenSink";

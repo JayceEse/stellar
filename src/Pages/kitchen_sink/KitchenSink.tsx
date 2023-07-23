f90da0278462a77c069b789d4c876b1bef84a646
Jayce Ese <jayce.ese@gmail.com>
2023-07-23 23:35:38 +1000

    style: class styles added to slides

--- src/Pages/kitchen_sink/KitchenSink.tsx
+++ src/Pages/kitchen_sink/KitchenSink.tsx
@@ -23,9 +23,25 @@ const KitchenSink = () => {
 
   // Background images slide
   const data = [
-    { img: HomeImg, label: "Home" },
-    { img: CoralIsland, label: "The Coral Island" },
-    { img: CrystalCaves, label: "The Crystal Caves" },
+    {
+      img: HomeImg,
+      label: "Go places you've dreamed of.",
+      description:
+        "Wafer dessert danish. Powder toffee cookie jelly beans bear claw jelly gingerbread halvah. and ",
+      temp: "",
+    },
+    {
+      img: CoralIsland,
+      label: "The Coral Island",
+      description: "The North Lake Iceland",
+      temp: "-16 ° C Very cold",
+    },
+    {
+      img: CrystalCaves,
+      label: "The Crystal Caves",
+      description: "The North Lake Iceland",
+      temp: "-16 ° C Very cold",
+    },
   ];
 
   interface ImgSlideStyle {
@@ -56,29 +72,61 @@ const KitchenSink = () => {
   const moreInfoStyle: string =
     "font-openSan font-bold text-white text-xs border-2 border-white border-solid w-40 h-9";
 
+  // Title & description styles
+  const titleBaseClasses: string =
+    "text-8xl font-noto text-white font-bold w-[600px] h-auto mb-20";
+
+  const titleHomeClasses: string =
+    "text-[150px] font-noto text-white font-bold w-[1300px] h-auto mb-8";
+
+  const paraHomeClasses: string =
+    "font-openSan text-2xl font-semibold text-white w-[810px] h-auto mb-11 mt-40";
+
+  const paraBaseClasses: string =
+    "font-openSan text-2xl font-semibold text-white w-40 h-14 mr-56";
+
+  const wrapperHomeClass: string = "flex flex-row justify-between w-auto";
+
+  const wrapperBaseClass: string = "flex flex-row w-auto";
+
+  const containerHomeClass: string =
+    "flex flex-col-reverse justify-between ml-36 mt-28 w-auto h-auto";
+
+  const containerBaseClass: string = "ml-36 mt-40";
+
   return (
     <>
       <div
         style={imgSlide}
-        className="flex flex-col justify-between duration-500"
+        className="flex flex-col justify-between duration-500 delay-75 mr-"
       >
         {/* Destination Info */}
-        <div className="ml-24 mt-20">
-          <div className="text-5xl font-noto text-white font-bold w-72 h-28 mb-8">
+        <div
+          className={
+            currentIndex === 0 ? containerHomeClass : containerBaseClass
+          }
+        >
+          <div
+            className={currentIndex === 0 ? titleHomeClasses : titleBaseClasses}
+          >
             <h1>{data[currentIndex].label}</h1>
           </div>
-          <div className="flex flex-row justify-between w-80">
-            <p className="font-openSan text-lg font-semibold text-white w-36 h-14">
-              The North Lake Iceland
+          <div
+            className={currentIndex === 0 ? wrapperHomeClass : wrapperBaseClass}
+          >
+            <p
+              className={currentIndex === 0 ? paraHomeClasses : paraBaseClasses}
+            >
+              {data[currentIndex].description}
             </p>
             <p className="flex fles-col items-end font-openSan text-sm font-normal text-white">
-              -16 ° C Very cold
+              {data[currentIndex].temp}
             </p>
           </div>
         </div>
 
         {/* Buttons */}
-        <div className="flex flex-row justify-between mx-24 mb-16">
+        <div className="flex flex-row justify-between mx-36 mb-16">
           {/* Slide buttons */}
           <div>
             <div className="flex flex-row justify-between w-28">

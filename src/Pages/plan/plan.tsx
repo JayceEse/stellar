798ab8b2be7d3b91f51e253041f844de8122e51e
Jayce Ese <jayce.ese@gmail.com>
2023-07-28 21:24:40 +1000

    first commit

--- src/Pages/plan/plan.tsx
+++ src/Pages/plan/plan.tsx
@@ -2,7 +2,7 @@ const Plan = () => {
   return (
     <>
       <div className="text-3xl w-screen h-screen flex items-center justify-center bg-cyan-500">
-        Plan a trip page
+        This is the plan page
       </div>
     </>
   );
